b59a210fedb144c8b2585d3b790da9a7072262c0
mikuhl <mikuhl@mail.uni-paderborn.de>
2017-06-17 18:05:55 +0200

    Update CNBO

--- MB-LR-SIC/cnbo.m
+++ MB-LR-SIC/cnbo.m
@@ -9,14 +9,15 @@ function [P] = cnbo(H)
 
     n = size(H,2);  % number of columns in H
     column_norms = zeros(1, n);
+    
     for k=1:length(column_norms)
         column_norms(k) = norm(H(:,k));  % Compute norms
         
-        addflops(flops_mul(H(:,k), H(:,k)) + flops_sqrt());
+        addflops(flops_mul(H(:,k)', H(:,k)) + flops_sqrt);
     end
     P = zeros(n, n); 
     for k=1:length(column_norms)
-        [argvalue, argmax] = max(column_norms);
+        [~, argmax] = max(column_norms);
         P(argmax, k) = 1;  % Permutate column k with column argmax
         column_norms(argmax) = 0;  % Ignore max_index for remaining 
                                    % iterations 
