49d2e63757293a7b9bdc8b25a03f882375d42440
mikuhl <mikuhl@mail.uni-paderborn.de>
2017-06-17 18:05:31 +0200

    Update CLLL algorithm

--- MB-LR-SIC/clll.m
+++ MB-LR-SIC/clll.m
@@ -1,4 +1,4 @@
-function [H_LR, T] = clll(H, delta)
+function [H_LR, T, P_c] = clll(H, delta)
 % CLLL Implements the complex LLL (CLLL) reduction algorithm [1]
 %
 %   Input  
@@ -15,35 +15,41 @@ function [H_LR, T] = clll(H, delta)
 %       Low Complexity Full-Diversity MIMO Detection", IEEE Transactions On
 %       Signal Processing, Vol. 57, No. 7, July 2009, pp. 2701 - 2710
 
+    test_arrival = 0;
+    test_passed = 0;
+
     if delta <= 1/2
         delta = 0.51;
     elseif delta > 1
             delta = 1;
     end
-
+    
     n = size(H, 2);  % Iterate over columns in H
     squared_norms = zeros(1, n);  % Denoted with calligraphic H in [1]. The 
-                                  % squared norms of the orthoganal vectors   
-    for k = 1:n
-        squared_norms(k) = dot(H(:,k), H(:,k)); addflops(6 * flops_mul(H(:,k), ... 
-            H(:,k)));
+                                  % squared norms of the original vectors   
+    for j = 1:n
+        squared_norms(j) = dot(H(:,j), H(:,j)); 
+        
+        addflops(flops_mul(H(:,j)',H(:,j)));
+        
     end
     
     % Modified Gram-Schmidt orthogonalization (GSO) procdure to compute the
     % squared norms
-    normed_prods = zeros(n,n);  % Denoted with \mu in [1]
-    for k = 1:n
-        for l = k+1:n 
-            inner_prod_kl = dot(H(:,l), H(:,k));
-            sub_term = sum(conj(normed_prods(k,1:k-1)) .* ...
-                normed_prods(l,1:k-1) .* squared_norms(1:k-1));
-            normed_prods(l,k) = (1 / squared_norms(k)) * ... 
-                (inner_prod_kl - sub_term);
-            squared_norms(l) = squared_norms(l) - ... 
-                abs(normed_prods(l, k))^2 * squared_norms(k);
+    mu = ones(n,n);  % Denoted with \mu in [1]. Initialize with zeros or 
+                     % with ones?
+    for j = 1:n
+        for i = j+1:n 
+            inner_prod_ij = dot(H(:,i), H(:,j));
+            sub_term = sum(conj(mu(j,1:j-1)) .* ...
+                mu(i,1:j-1) .* squared_norms(1:j-1));
+            mu(i,j) = (1 / squared_norms(j)) * ... 
+                (inner_prod_ij - sub_term);
+            squared_norms(i) = squared_norms(i) - ... 
+                abs(mu(i, j))^2 * squared_norms(j);
             
-            addflops(6 * flops_mul(H(:,l), H(:,k)) + 12 * (k-1) + ... 
-                flops_row_sum(1, k-1) + flops_div() + 4 + flops_abs() + ... 
+            addflops(flops_mul(H(:,i)', H(:,j)) + 2 * (j-1) + ... 
+                flops_row_sum(1, j-1) + flops_div() + 4 + flops_abs() + ... 
                 flops_pow(2) + 2);
         end
     end
@@ -51,15 +57,14 @@ function [H_LR, T] = clll(H, delta)
     T = eye(n);
     k = 2;
     while k <= n
-        if abs(real(normed_prods(k,k-1))) > 1/2 || ... 
-                abs(imag(normed_prods(k,k-1))) > 1/2
+        if abs(real(mu(k,k-1))) > 1/2 || abs(imag(mu(k,k-1))) > 1/2
             % First condition (5) for a CLLL-reduced complex lattice is  
             % violated -> Reduce vector size 
             addflops(2 * flops_abs() + 4);
             
-            [H, T, normed_prods] = size_reduce(H, T, normed_prods, k, k-1);
+            [H, T, mu] = size_reduce(H, T, mu, k, k-1);
         end
-        if squared_norms(k) < (delta - abs(normed_prods(k,k-1))^2) * ... 
+        if squared_norms(k) < (delta - abs(mu(k,k-1))^2) * ... 
                 squared_norms(k-1)
             % Second condition (6) for a CLLL-reduced complex lattice is
             % violated -> Swap vectors and update ((7) - (15) in [1])
@@ -68,47 +73,50 @@ function [H_LR, T] = clll(H, delta)
             H(:,[k,k-1]) = H(:,[k-1,k]);  % (7) and (8)
             H_k = squared_norms(k);
             H_k_1 = squared_norms(k-1);
-            squared_norms(k-1) = H_k + abs(normed_prods(k,k-1))^2 * ... 
+            squared_norms(k-1) = H_k + abs(mu(k,k-1))^2 * ... 
                 H_k_1;  % (9)
-            normed_prods(k,k-1) = conj(normed_prods(k,k-1)) * ... 
+            mu_old = mu;
+            mu(k,k-1) = conj(mu(k,k-1)) * ... 
                 (H_k_1 / squared_norms(k-1));  % (10)
-            squared_norms(k) = H_k_1 - abs(normed_prods(k,k-1))^2 * ... 
+            squared_norms(k) = H_k_1 - abs(mu(k,k-1))^2 * ... 
                 squared_norms(k-1);  % (11)
             
             addflops(1 + flops_abs() + flops_pow(2) + 1 + flops_div() + ... 
                 2 + 1 + flops_abs() + flops_pow(2) + 1);
             
-            for l = k+1:n
-                tmp = normed_prods(l,k-1);
-                normed_prods(l,k-1) = normed_prods(l,k-1) * ... 
-                    normed_prods(k,k-1) + normed_prods(l,k) * ... 
+            for i = k+1:n
+                mu(i,k-1) = mu_old(i,k-1) * ... 
+                    mu(k,k-1) + mu_old(i,k) * ... 
                     (H_k / squared_norms(k-1));  % (12)
-                normed_prods(l,k) = tmp - normed_prods(l,k) * ... 
-                    normed_prods(k, k-1);  % (13)
+                mu(i,k) = mu_old(i,k-1) - mu_old(i,k) * ... 
+                    mu_old(k, k-1);  % (13)
+                
+                addflops(flops_div() + 5);
             end
-            addflops((n - (k+1)) * 18);
             
-            for l = 1:k-2
-                normed_prods([k-1,k],l) = normed_prods([k,k-1],l);  % (14)
-                % and (15)
+            for j = 1:k-2
+                mu(k-1,j) = mu_old(k,j);  % (14) 
+                mu(k,j) = mu_old(k-1,j);  % (15)
             end
             T(:,[k,k-1]) = T(:,[k-1,k]);
             k = max(2,k-1);
         else
-            for l = k-2:-1:1
-                if abs(real(normed_prods(k,l))) > 1/2 || ... 
-                        abs(imag(normed_prods(k,l))) > 1/2
+            for j = k-2:-1:1
+                test_arrival = test_arrival + 1;
+                if abs(real(mu(k,j))) > 1/2 || abs(imag(mu(k,j))) > 1/2
                     % First condition (5) for a CLLL-reduced complex
                     % lattice is violated -> Reduce vector size
                     addflops(2 * flops_abs() + 4);
+                    test_passed = test_passed + 1;
                     
-                    [H, T, normed_prods] = ... 
-                        size_reduce(H, T, normed_prods, k, l);
+                    [H, T, mu] = size_reduce(H, T, mu, k, j);
                 end
             end
             k = k+1;
         end
     end
     H_LR = H;
+    P_c = test_passed / test_arrival;  % Probability that conditional test 
+                                       % is passed
     
 end
