c13d3826d85dd1ecd4ec961efb96faaa560bcd36
mikuhl <mikuhl@mail.uni-paderborn.de>
2017-09-06 18:23:18 +0200

    Update simulation script

--- MB-LR-SIC/simulation/numerical_results.m
+++ MB-LR-SIC/simulation/numerical_results.m
@@ -15,9 +15,6 @@ function numerical_results(K,N_txi,scenario,var_s,modulation,runs)
 
 addpath('../algorithm');
 
-% K = 3;  % number of active user
-
-% N_txi = 2;  % number of antennas for user i
 N_t = K*N_txi;  % total number of transmit antennas
 N_r = N_t;  % number of total receive antennas 
 
@@ -27,20 +24,12 @@ N_r = N_t;  % number of total receive antennas
 
 % Parameters for channel scenario 2
 L_p = 0.7;  % Base power path loss
-sigma = 6;  % shadowing spread in dB
+% sigma = 6;  % shadowing spread in dB
 correlation_tx = 0.2;  % correlation index of neighboring transmit antennas
 correlation_rx = correlation_tx;  % correlation index of neighboring receive
                                   % antennas
 
-channel_estimation = false;  % If false, assume perfectly known channel
-
-% modulation = 'QPSK';  % 'QPSK', '16-QAM'
-
-% var_s = 1;  % Signal power
-
-% runs = 10;
-train_symbols = 50*N_t;  % Number of symbols used for channel estimation
-transmitted_symbols = 50*N_t;
+transmitted_symbols = 500;
 
 snr_step = 5;
 
@@ -72,23 +61,13 @@ elseif strcmp(modulation, 'QPSK')
 else
     error('Invalid modulation %s', modulation);
 end
-                     
-if channel_estimation == true
-    transmitted_symbols = transmitted_symbols + train_symbols;
-end
 
 R_tx = correlation_tx.^(((0:-1:-(N_t-1))'+(0:N_t-1)).^2);  % Correlation matrix
 % for transmit antennas
 R_rx = correlation_rx.^(((0:-1:-(N_r-1))'+(0:N_r-1)).^2);  % Correlation matrix
 % for receive antennas
 alpha_k = sqrt(L_p);
-beta_k = 10.^(sigma*randn(N_r,N_t)/10);
-
-if channel_estimation == true
-    train_signal = constellation(mod(0:N_t-1,numel(constellation))+1)';  
-    % Signal used for channel estimation 
-end
-                                   
+                           
 BER_mblrsic = zeros(1, SNR_max/snr_step+1);
 BER_mbsic = zeros(1, SNR_max/snr_step+1);  % MB-SIC detector
 BER_lrsic = zeros(1, SNR_max/snr_step+1);  % LR-SIC detector
@@ -111,12 +90,12 @@ for SNR = 0:snr_step:SNR_max
     BERs_ml = zeros(1, runs);
     
     for i = 1:runs  % simulation runs
-        H = 1/sqrt(2) * (randn(N_r, N_t) + 1j * randn(N_r, N_t)); 
+        H = randn(N_r, N_t) + 1j * randn(N_r, N_t); 
         % channels for scenario 1
         
         if scenario == 2
             G = sqrtm(R_rx)*H*sqrtm(R_tx);
-            H = alpha_k*beta_k.*G;
+            H = alpha_k*G;
         end
         
         BE_mblrsic = 0;  % Bit Errors for MB-LR-SIC detector
@@ -126,68 +105,53 @@ for SNR = 0:snr_step:SNR_max
         BE_lrmmse = 0;
         BE_ml = 0;  % Bit Errors for ML detector
         
-        H_orig = H;
-
         for k = 1:transmitted_symbols
             
             n = sqrt(N/2) * (randn(N_r,1)+1j*randn(N_r,1));  % noise vector
             
-            if k <= train_symbols && channel_estimation == true
-                y = H_orig*train_signal + n;
-                
-                if k == 1
-                    [H, D, P] = ls_channel_estimation_mimo(y, ... 
-                        train_signal, 0.75, 1e-6);
-                else
-                    [H, D, P] = ls_channel_estimation_mimo(y, ...
-                        train_signal, 0.75, 1e-6, D, P);
-                end
-                
-            else
-            
-                s = constellation(randi([1,numel(constellation)], 1, N_t))';  
-                % Generate signal vector
+            s = constellation(randi([1,numel(constellation)], 1, N_t))';  
+            % Generate signal vector
 
-                b = decode(s, constellation);  % Transmitted bits
+            b = decode(s, constellation);  % Transmitted bits
 
-                y = H * s + n;
+            y = H * s + n;
 
-                if mblrsic_dec == true
-                    s_mblrsic = mb_lr_sic(y,H,var_s,N,modulation,constellation);
-                    b_mblrsic = decode(s_mblrsic, constellation);
-                    BE_mblrsic = BE_mblrsic + sum(b ~= b_mblrsic);
-                end
-
-                if lr_sic_dec == true
-                    s_lrsic = lr_sic(y,H,var_s,N,modulation,constellation,true);
-                    b_lrsic = decode(s_lrsic, constellation);
-                    BE_lrsic = BE_lrsic + sum(b ~= b_lrsic);
-                end
-
-                if mbsic_dec == true
-                    s_mbsic = mb_lr_sic(y,H,var_s,N,modulation,constellation,false);
-                    b_mbsic = decode(s_mbsic, constellation);
-                    BE_mbsic = BE_mbsic + sum(b ~= b_mbsic);
-                end
-
-                if sic_dec == true
-                    s_sic = lr_sic(y,H,var_s,N,modulation,constellation,false);
-                    b_sic = decode(s_sic, constellation);
-                    BE_sic = BE_sic + sum(b ~= b_sic);
-                end
-
-                if lr_mmse_dec == true
-                    s_lrmmse = lr_mmse(y,H,var_s,N,modulation,constellation,true);
-                    b_lrmmse = decode(s_lrmmse, constellation);
-                    BE_lrmmse = BE_lrmmse + sum(b ~= b_lrmmse);
-                end
-
-                if ml_detection == true
-                    s_ml = ml_mimo(y,H,constellation);
-                    b_ml = decode(s_ml, constellation);
-                    BE_ml = BE_ml + sum(b ~= b_ml);
-                end
+            if mblrsic_dec == true
+                s_mblrsic = mb_lr_sic(y,H,var_s,N,modulation,constellation,true,true);
+                b_mblrsic = decode(s_mblrsic, constellation);
+                BE_mblrsic = BE_mblrsic + sum(b ~= b_mblrsic);
             end
+
+            if lr_sic_dec == true
+                s_lrsic = lr_sic(y,H,var_s,N,modulation,constellation,true,true);
+                b_lrsic = decode(s_lrsic, constellation);
+                BE_lrsic = BE_lrsic + sum(b ~= b_lrsic);
+            end
+
+            if mbsic_dec == true
+                s_mbsic = mb_lr_sic(y,H,var_s,N,modulation,constellation,false,false);
+                b_mbsic = decode(s_mbsic, constellation);
+                BE_mbsic = BE_mbsic + sum(b ~= b_mbsic);
+            end
+
+            if sic_dec == true
+                s_sic = lr_sic(y,H,var_s,N,modulation,constellation,false,false);
+                b_sic = decode(s_sic, constellation);
+                BE_sic = BE_sic + sum(b ~= b_sic);
+            end
+
+            if lr_mmse_dec == true
+                s_lrmmse = lr_mmse(y,H,var_s,N,modulation,constellation,true);
+                b_lrmmse = decode(s_lrmmse, constellation);
+                BE_lrmmse = BE_lrmmse + sum(b ~= b_lrmmse);
+            end
+
+            if ml_detection == true
+                s_ml = ml_mimo(y,H,constellation);
+                b_ml = decode(s_ml, constellation);
+                BE_ml = BE_ml + sum(b ~= b_ml);
+            end
+            
         end
         
         BERs_mblrsic(i) = BE_mblrsic / (transmitted_symbols * N_t * ... 
@@ -256,7 +220,7 @@ if mbsic_dec == true
     hold on
 end
 if lr_sic_dec == true
-    semilogy(0:snr_step:SNR_max, BER_lrsic, 'b-p', ... 
+    semilogy(0:snr_step:SNR_max, BER_lrsic, 'b-+', ... 
         'LineWidth', 2, ...
         'DisplayName', 'LR-SIC');
     hold on
@@ -280,7 +244,7 @@ if ml_detection == true
 end
 
 title(sprintf('MU-MIMO System, $N_{txi}$=%d, $N_r$=%d, L=%d Branches and K=%d users', ...
-    N_txi, N_r, N_t, K));
+    N_txi, N_r, N_t+1, K));
 
 xlabel('SNR [dB]');
 xticks(0:snr_step:SNR_max);
